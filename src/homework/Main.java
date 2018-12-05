0d53a6d48ec87a37d883426cd01f4e07b1c899ae
Oleksii Shchelkonohov <jobmanalex@gmail.com>
2018-12-05 17:21:13 +0200

    Skidka done

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -3,8 +3,28 @@ package homework;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int total = 600;
+		int discount = 0;
+		boolean a = total > 400;
+		switch (total / 100) {
+		case 0:
+		case 1:
+		case 2:
+			discount = 0;
+			break;
+		case 3:
+		case 4:
+		case 5:
+			discount = 3;
+			break;
+		default:
+			discount = 5;
+			break;
 
+		}
+		int toPay = total-(total*discount/100);
+		System.out.println("Total: " + total + " discount " + discount + "%, to pay " + toPay);
+		// comment
 	}
 
 }
