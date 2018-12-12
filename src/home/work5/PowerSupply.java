f34527808149cac959734058d93829044b0d45ce
Oleksii Shchelkonohov <jobmanalex@gmail.com>
2018-12-12 19:15:42 +0200

    format & modificator of accsses

--- src/home/work5/PowerSupply.java
+++ src/home/work5/PowerSupply.java
@@ -1,35 +1,35 @@
 package home.work5;
 
 public class PowerSupply {
-	boolean isEnable;
-	int inVoltage;
-	int outVoltage1;
-	int outVoltage2;
-	
+	private boolean isEnable;
+	private int inVoltage;
+	private int outVoltage1;
+	private int outVoltage2;
+
 	public void printInfo() {
-		System.out.println("Power supply is "+(isEnable?"ON":"OFF"));
-		System.out.println("Incomming voltage "+inVoltage+" V");
-		System.out.println("Out 1 voltage "+outVoltage1+" V");
-		System.out.println("Out 2 voltage "+outVoltage2+" V");
+		System.out.println("Power supply is " + (isEnable ? "ON" : "OFF"));
+		System.out.println("Incomming voltage " + inVoltage + " V");
+		System.out.println("Out 1 voltage " + outVoltage1 + " V");
+		System.out.println("Out 2 voltage " + outVoltage2 + " V");
 	}
-	
+
 	public void energize(int volts) {
-		if(volts>250) {
-			isEnable=false;
-			inVoltage=volts;
-			outVoltage1=0;
-			outVoltage2=0;
+		if (volts > 250) {
+			isEnable = false;
+			inVoltage = volts;
+			outVoltage1 = 0;
+			outVoltage2 = 0;
 			System.out.println("High Voltage");
-		} else if(volts>200) {
-			isEnable=true;
-			inVoltage=volts;
-			outVoltage1=12;
-			outVoltage2=5;
+		} else if (volts > 200) {
+			isEnable = true;
+			inVoltage = volts;
+			outVoltage1 = 12;
+			outVoltage2 = 5;
 		} else {
-			isEnable=false;
-			inVoltage=volts;
-			outVoltage1=0;
-			outVoltage2=0;
+			isEnable = false;
+			inVoltage = volts;
+			outVoltage1 = 0;
+			outVoltage2 = 0;
 			System.out.println("Low Voltage");
 		}
 	}
