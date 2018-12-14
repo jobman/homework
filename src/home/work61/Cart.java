ccd0e4e638536e50e26bcbc343428d229fd074a5
Oleksii Shchelkonohov <jobmanalex@gmail.com>
2018-12-14 14:40:07 +0200

    Cart done

---
+++ src/home/work61/Cart.java
@@ -0,0 +1,46 @@
+package home.work61;
+
+
+public class Cart {
+
+	private Product[] setOfProducts = new Product[10];
+	private int countOfProducts;
+
+	public void addProduct(Product product) {
+		if (countOfProducts < 10) {
+			for (int i = 0; i < setOfProducts.length; i++) {
+				if (setOfProducts[i] == null) {
+					setOfProducts[i] = product;
+					countOfProducts++;
+					break;
+				}
+			}
+		} else {
+			System.out.println("Error");
+		}
+	}
+
+	public void getInfo() {
+		int total = 0;
+		if(countOfProducts>0) {
+			for (int i = 0; i < setOfProducts.length; i++) {
+				if (setOfProducts[i] != null) {
+					System.out.println(setOfProducts[i].getName());
+					total+=setOfProducts[i].getPrice();
+				}
+			}
+		} else {
+			System.out.println("Empty cart");
+		}
+		System.out.println("Total: "+total);
+	}
+
+	public void deleteProductByName(String name) {
+		for (int i = 0; i < setOfProducts.length; i++) {
+			if ((setOfProducts[i] != null) && (setOfProducts[i].getName().contains(name))) {
+				setOfProducts[i] = null;
+				countOfProducts--;
+			}
+		}
+	}
+}
--- src/home/work61/MainClass.java
+++ src/home/work61/MainClass.java
@@ -3,7 +3,26 @@ package home.work61;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Cart cart = new Cart();
+		Product product1 = new Product("Bread", 13);
+		Product product2 = new Product("Bread", 10);
+		Product product3 = new Product("Semki", 10);
+		Product product4 = new Product("Milk", 5);
+		Product product5 = new Product("Milk", 5);
+
+		cart.getInfo();
+		System.out.println();
+
+		cart.addProduct(product1);
+		cart.addProduct(product2);
+		cart.addProduct(product3);
+		cart.addProduct(product4);
+		cart.addProduct(product5);
+		cart.getInfo();
+		System.out.println();
+
+		cart.deleteProductByName("Bread");
+		cart.getInfo();
 
 	}
 
